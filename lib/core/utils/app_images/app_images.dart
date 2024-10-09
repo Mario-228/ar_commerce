5e187eb090c4be0f425ea2b9a4a97efbc8aba3c2
MarioYoussef <marioyoussef08@gmail.com>
2024-10-09 21:04:03 +0300

    add app images class

---
+++ lib/core/utils/app_images/app_images.dart
@@ -0,0 +1,16 @@
+// ignore_for_file: prefer_single_quotes
+abstract class AppImages {
+  AppImages._();
+
+  /// Assets for assetsImagesBigChair
+  /// assets/images/big_chair.svg
+  static const String assetsImagesBigChair = "assets/images/big_chair.svg";
+
+  /// Assets for assetsImagesLogo
+  /// assets/images/logo.png
+  static const String assetsImagesLogo = "assets/images/logo.png";
+
+  /// Assets for assetsImagesSmallChair
+  /// assets/images/small_chair.svg
+  static const String assetsImagesSmallChair = "assets/images/small_chair.svg";
+}
