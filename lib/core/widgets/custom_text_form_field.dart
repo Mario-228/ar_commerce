ef618d9c55b75fafff94c3020038e60378bbf05a
MarioYoussef <marioyoussef08@gmail.com>
2024-10-11 22:09:00 +0300

    create custom text form field

---
+++ lib/core/widgets/custom_text_form_field.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:graduation_project/core/utils/app_colors/app_colors.dart';
+import 'package:graduation_project/core/utils/font_styles/font_styles.dart';
+
+class CustomTextFormField extends StatelessWidget {
+  const CustomTextFormField({
+    super.key,
+    required this.type,
+    required this.labelText,
+    required this.controller,
+    this.validator,
+    this.icon,
+    this.onPressed,
+    this.isPassword = false,
+  });
+  final TextInputType type;
+  final String labelText;
+  final TextEditingController controller;
+  final String? Function(String?)? validator;
+  final IconData? icon;
+  final void Function()? onPressed;
+  final bool isPassword;
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      obscureText: isPassword,
+      keyboardType: type,
+      decoration: InputDecoration(
+        border: const OutlineInputBorder(
+          borderSide: BorderSide(color: AppColors.lightGrey),
+          borderRadius: BorderRadius.all(
+            Radius.circular(6.0),
+          ),
+        ),
+        labelText: labelText,
+        labelStyle:
+            FontStyles.textStyleLight13.copyWith(fontWeight: FontWeight.w400),
+        suffixIcon: IconButton(onPressed: onPressed, icon: Icon(icon)),
+      ),
+      controller: controller,
+      validator: validator,
+    );
+  }
+}
