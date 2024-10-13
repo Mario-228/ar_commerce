a62ac52df71334793559b604fb443dd80f76ed7d
Kyria-Ashraf <keirayaashraf@gmail.com>
2024-10-13 03:26:03 +0300

    change login header and footer

--- lib/core/widgets/custom_footer_widget.dart
+++ lib/core/widgets/custom_footer_widget.dart
@@ -2,26 +2,34 @@ import 'package:flutter/material.dart';
 import 'package:graduation_project/core/utils/app_colors/app_colors.dart';
 import 'package:graduation_project/core/utils/font_styles/font_styles.dart';
 import 'package:graduation_project/core/widgets/custom_material_button.dart';
+import 'package:graduation_project/features/signup_feature/presentation/views/signup_view.dart';
 
-class LoginFooterWidget extends StatelessWidget {
-  const LoginFooterWidget({
+class CustomFooterWidget extends StatelessWidget {
+  const CustomFooterWidget({
     super.key,
+    required this.buttonTitle,
+    required this.footerText,
+    required this.footerLinkText,
+    required this.onPressed,
   });
+  final String buttonTitle;
+  final String footerText;
+  final String footerLinkText;
+  final void Function() onPressed;
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         CustomMaterialButton(
-            text: "Login", color: AppColors.darkGreen, onPressed: () {}),
+            text: buttonTitle, color: AppColors.darkGreen, onPressed: () {}),
         const SizedBox(height: 20.0),
         Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-          const Text("Don't have an account? ",
-              style: FontStyles.textStyleMedium12),
+          Text(footerText, style: FontStyles.textStyleMedium12),
           GestureDetector(
-            onTap: () {},
+            onTap: onPressed,
             child: Text(
-              "Create Account Here",
+              footerLinkText,
               style: FontStyles.textStyleMedium12.copyWith(
                   fontWeight: FontWeight.w600, color: AppColors.orange),
             ),
