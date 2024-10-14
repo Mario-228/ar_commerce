b85a233be857a0902cf8a323123a98daddf2abf9
MarioYoussef <marioyoussef08@gmail.com>
2024-10-14 15:52:10 +0300

    adding app routers to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
-import 'package:graduation_project/features/login_feature/presentation/views/login_view.dart';
+import 'package:graduation_project/core/utils/app_routers/app_routers.dart';
+// import 'package:graduation_project/features/login_feature/presentation/views/login_view.dart';
 // import 'package:graduation_project/features/onboarding_view/presentation/views/onboarding_view.dart';
 // import 'package:graduation_project/features/splash_view/presentation/views/splash_view.dart';
 
@@ -15,14 +16,14 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routerConfig: AppRouters.routers,
       debugShowCheckedModeBanner: false,
       locale: DevicePreview.locale(context),
       builder: DevicePreview.appBuilder,
       theme: ThemeData(
         useMaterial3: true,
       ),
-      home: const LoginView(),
     );
   }
 }
