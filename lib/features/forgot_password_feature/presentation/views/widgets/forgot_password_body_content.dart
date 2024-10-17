b1090452628c92fd9356b7b6c43b6186997aec18
MarioYoussef <marioyoussef08@gmail.com>
2024-10-17 04:08:15 +0300

    finish configuration forget password

--- lib/features/forgot_password_feature/presentation/views/widgets/forgot_password_body_content.dart
+++ lib/features/forgot_password_feature/presentation/views/widgets/forgot_password_body_content.dart
@@ -1,3 +1,4 @@
+import 'package:email_validator/email_validator.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:graduation_project/core/utils/app_colors/app_colors.dart';
@@ -13,28 +14,44 @@ class ForgotPasswordBodyContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      mainAxisSize: MainAxisSize.max,
-      children: [
-        const SizedBox(height: 30.0),
-        const CustomHeaderWidget(
-            title: "Forgot Password",
-            subtitle: "Login to your minifurs account"),
-        const SizedBox(height: 40.0),
-        CustomTextFormField(
-            type: TextInputType.emailAddress,
-            labelText: "Enter Email Address",
-            controller: TextEditingController()),
-        const SizedBox(height: 35.0),
-        CustomMaterialButton(
-          text: "Send Email",
-          color: AppColors.darkGreen,
-          onPressed: () =>
-              GoRouter.of(context).push(AppRouters.kPasswordResetView),
-          textColor: AppColors.white,
-        ),
-      ],
+    GlobalKey<FormState> forgetPasswordFormKey = GlobalKey<FormState>();
+    return Form(
+      key: forgetPasswordFormKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          const SizedBox(height: 30.0),
+          const CustomHeaderWidget(
+              title: "Forgot Password",
+              subtitle: "Login to your minifurs account"),
+          const SizedBox(height: 40.0),
+          CustomTextFormField(
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Enter your email';
+                } else if (!(EmailValidator.validate(value))) {
+                  return "Insert valid email";
+                } else {
+                  return null;
+                }
+              },
+              type: TextInputType.emailAddress,
+              labelText: "Enter Email Address",
+              controller: TextEditingController()),
+          const SizedBox(height: 35.0),
+          CustomMaterialButton(
+            text: "Send Email",
+            color: AppColors.darkGreen,
+            onPressed: () {
+              if (forgetPasswordFormKey.currentState!.validate()) {
+                GoRouter.of(context).push(AppRouters.kPasswordResetView);
+              }
+            },
+            textColor: AppColors.white,
+          ),
+        ],
+      ),
     );
   }
 }
