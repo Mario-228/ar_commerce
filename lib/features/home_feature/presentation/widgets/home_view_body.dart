977ac4b14a140a03414aac4f5a271f5478899156
MarioYoussef <marioyoussef08@gmail.com>
2024-10-22 15:38:03 +0300

    add products to home view

--- lib/features/home_feature/presentation/widgets/home_view_body.dart
+++ lib/features/home_feature/presentation/widgets/home_view_body.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:graduation_project/features/home_feature/presentation/widgets/custom_latest_release.dart';
+import 'package:graduation_project/features/home_feature/presentation/widgets/custom_products_grid_view.dart';
 import 'package:graduation_project/features/home_feature/presentation/widgets/home_categories_list.dart';
 import 'package:graduation_project/features/home_feature/presentation/widgets/home_view_header.dart';
 
@@ -10,6 +10,7 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         ClipRRect(
           borderRadius: BorderRadius.only(
@@ -18,15 +19,21 @@ class HomeViewBody extends StatelessWidget {
           ),
           child: HomeViewHeader(),
         ),
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 20.0),
-          child: Column(
-            children: [
-              SizedBox(height: 40.0),
-              HomeCategoriesList(),
-              SizedBox(height: 20.0),
-              CustomLatestRelease(),
-            ],
+        Expanded(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
+              child: Column(
+                children: [
+                  SizedBox(height: 40.0),
+                  HomeCategoriesList(),
+                  SizedBox(height: 20.0),
+                  CustomLatestRelease(),
+                  SizedBox(height: 20.0),
+                  CustomProductsGridView(),
+                ],
+              ),
+            ),
           ),
         )
       ],
