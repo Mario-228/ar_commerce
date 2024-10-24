109a6429627fafb5dea912ca97315b539e3c8b0a
Kyria-Ashraf <keirayaashraf@gmail.com>
2024-10-24 08:53:27 +0300

    create product details screen

---
+++ lib/features/product_details_feature/presentation/views/product_details_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:graduation_project/features/product_details_feature/presentation/views/widgets/product_details_view_body.dart';
+
+class ProductDetailsView extends StatelessWidget {
+  const ProductDetailsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const ProductDetailsViewBody();
+  }
+}
