c8a394406e7d926e4497091541d7d971530bab1c
Kyria-Ashraf <keirayaashraf@gmail.com>
2024-10-24 08:59:21 +0300

    create add to cart and AR view buttons

---
+++ lib/features/product_details_feature/presentation/views/widgets/product_details_view_footer.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:graduation_project/core/utils/app_colors/app_colors.dart';
+import 'package:graduation_project/core/utils/font_styles/font_styles.dart';
+
+class ProductDetailsViewFooter extends StatelessWidget {
+  const ProductDetailsViewFooter({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        MaterialButton(
+          onPressed: () {},
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.0),
+              side: const BorderSide(color: AppColors.black)),
+          child: const Text("Add to cart", style: FontStyles.textStyleMedium12),
+        ),
+        const SizedBox(
+          width: 20.0,
+        ),
+        MaterialButton(
+          onPressed: () {},
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          color: AppColors.darkGreen,
+          child: Text(
+            "Set in your space",
+            style:
+                FontStyles.textStyleMeduim13.copyWith(color: AppColors.white),
+          ),
+        )
+      ],
+    );
+  }
+}
