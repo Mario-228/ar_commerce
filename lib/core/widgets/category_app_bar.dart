20981bd4a19d9231fc39213b420b5b446ddb0b3b
Kyria-Ashraf <keirayaashraf@gmail.com>
2024-11-18 18:52:28 +0200

    moved category app bar to core - widgets and added title parameter to widget

--- lib/core/widgets/category_app_bar.dart
+++ lib/core/widgets/category_app_bar.dart
@@ -1,22 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:graduation_project/core/utils/app_colors/app_colors.dart';
 import 'package:graduation_project/core/utils/font_styles/font_styles.dart';
 
 class CategoryAppbar extends StatelessWidget implements PreferredSizeWidget {
-  const CategoryAppbar({super.key});
-
+  const CategoryAppbar({super.key, required this.title});
+  final String title;
   @override
   Widget build(BuildContext context) {
     return AppBar(
       shadowColor: AppColors.lightGrey,
       backgroundColor: AppColors.white,
-      title: const Text(
-        "Chair",
+      title: Text(
+        title,
         style: FontStyles.textStyleBold22,
       ),
       centerTitle: true,
       leading: IconButton(
-          icon: const Icon(Icons.arrow_back_ios_new), onPressed: () {}),
+          icon: const Icon(Icons.arrow_back_ios_new),
+          onPressed: () {
+            GoRouter.of(context).pop();
+          }),
       actions: [
         IconButton(
           onPressed: () {},
