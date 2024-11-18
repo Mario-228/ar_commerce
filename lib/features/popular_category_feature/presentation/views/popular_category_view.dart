8ff8d7a824018f650fa267469106904fc90f3235
Kyria-Ashraf <keirayaashraf@gmail.com>
2024-11-18 18:49:52 +0200

    created popular category view

---
+++ lib/features/popular_category_feature/presentation/views/popular_category_view.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:graduation_project/core/widgets/category_app_bar.dart';
+import 'package:graduation_project/core/widgets/custom_bottom_navigation_bar.dart';
+import 'package:graduation_project/features/furniture_category_feature/presentation/views/widgets/furniture_grid_view.dart';
+
+class PopularCategoryView extends StatelessWidget {
+  const PopularCategoryView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      appBar: CategoryAppbar(
+        title: "Popular",
+      ),
+      body: FurnitureProductsGridView(),
+      bottomNavigationBar: CustomBottomNavigationBar(),
+    );
+  }
+}
