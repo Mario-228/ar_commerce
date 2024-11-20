53401d924f6db84d54dadff23f8a0ee6a8b06e78
MarioYoussef <marioyoussef08@gmail.com>
2024-11-20 14:57:13 +0200

    create api sevice class and end points class

---
+++ lib/core/utils/api_service/api_end_points.dart
@@ -0,0 +1,7 @@
+abstract class ApiEndPoints {
+  static const String resesPassword = "/reset/password";
+  static const String register = "/register";
+  static const String login = "/login";
+  static const String logout = "/logout";
+  static const String user = "/user";
+}
--- lib/features/popular_category_feature/presentation/views/popular_category_view.dart
+++ lib/features/popular_category_feature/presentation/views/popular_category_view.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:graduation_project/core/widgets/category_app_bar.dart';
 import 'package:graduation_project/core/widgets/custom_bottom_navigation_bar.dart';
-import 'package:graduation_project/features/furniture_category_feature/presentation/views/widgets/furniture_grid_view.dart';
 import 'package:graduation_project/features/popular_category_feature/presentation/views/widgets/popular_grid_view.dart';
 
 class PopularCategoryView extends StatelessWidget {
