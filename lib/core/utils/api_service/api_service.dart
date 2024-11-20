826005f8cd658f9c6f5c2dc4b1bb00f8daceabd8
MarioYoussef <marioyoussef08@gmail.com>
2024-11-20 15:04:46 +0200

    modify api service class

--- lib/core/utils/api_service/api_service.dart
+++ lib/core/utils/api_service/api_service.dart
@@ -4,17 +4,19 @@ class ApiService {
   static const String baseUrl =
       "https://e807-102-190-243-167.ngrok-free.app/api";
 
-  static final Dio dioHelper = Dio(BaseOptions(
-    baseUrl: baseUrl,
-    receiveDataWhenStatusError: true,
-  ));
+  static final Dio dioHelper = Dio(
+    BaseOptions(
+      baseUrl: baseUrl,
+      receiveDataWhenStatusError: true,
+    ),
+  );
 
-  Future<Map<String, dynamic>> get(String endPoint) async {
+  static Future<Map<String, dynamic>> get(String endPoint) async {
     var response = await dioHelper.get(endPoint);
     return response.data;
   }
 
-  Future<Map<String, dynamic>> postData(
+  static Future<Map<String, dynamic>> postData(
       String endPoint, Map<String, dynamic> query) async {
     var response = await dioHelper.post(endPoint, queryParameters: query);
     return response.data;
