7dd63544d8851126ef2015561a61ca3adc000a77
MarioYoussef <marioyoussef08@gmail.com>
2024-12-05 01:08:08 +0200

    trigger home get product cubit

--- lib/features/home_feature/presentation/widgets/custom_home_products_grid_view.dart
+++ lib/features/home_feature/presentation/widgets/custom_home_products_grid_view.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 // import 'package:go_router/go_router.dart';
 // import 'package:graduation_project/core/utils/app_routers/app_routers.dart';
 import 'package:graduation_project/core/utils/custom_product_item_model/custom_product_item_model.dart';
+import 'package:graduation_project/features/home_feature/presentation/views_models/get_product_cubit/get_product_cubit.dart';
+import 'package:graduation_project/features/home_feature/presentation/views_models/get_product_cubit/get_product_states.dart';
 import 'package:graduation_project/features/home_feature/presentation/widgets/custom_product_item.dart';
 
 class CustomHomeProductsGridView extends StatelessWidget {
@@ -9,76 +12,31 @@ class CustomHomeProductsGridView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    List<CustomProductItemModel> productItems = [
-      CustomProductItemModel(
-        id: 1,
-        name: 'Product 1',
-        price: 10,
-        pictureUrl: 'https://via.placeholder.com/150',
-        isFavorite: true,
-        productBrand: 'Brand 1',
-        productBrandId: 1,
-        quantity: 10,
-        productTypeId: 1,
-        productType: 'Type 1',
-        currency: 'L.E ',
-        description: 'Description 1',
-      ),
-      CustomProductItemModel(
-        id: 1,
-        name: 'Product 1',
-        price: 10,
-        pictureUrl: 'https://via.placeholder.com/150',
-        isFavorite: true,
-        productBrand: 'Brand 1',
-        productBrandId: 1,
-        quantity: 10,
-        productTypeId: 1,
-        productType: 'Type 1',
-        currency: 'L.E ',
-        description: 'Description 1',
-      ),
-      CustomProductItemModel(
-        id: 1,
-        name: 'Product 1',
-        price: 10,
-        pictureUrl: 'https://via.placeholder.com/150',
-        isFavorite: true,
-        productBrand: 'Brand 1',
-        productBrandId: 1,
-        quantity: 10,
-        productTypeId: 1,
-        productType: 'Type 1',
-        currency: 'L.E ',
-        description: 'Description 1',
-      ),
-      CustomProductItemModel(
-        id: 1,
-        name: 'Product 1',
-        price: 10,
-        pictureUrl: 'https://via.placeholder.com/150',
-        isFavorite: true,
-        productBrand: 'Brand 1',
-        productBrandId: 1,
-        quantity: 10,
-        productTypeId: 1,
-        productType: 'Type 1',
-        currency: 'L.E ',
-        description: 'Description 1',
-      ),
-    ];
-    return GridView.builder(
-      shrinkWrap: true,
-      physics: const NeverScrollableScrollPhysics(),
-      itemCount: productItems.length,
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        crossAxisSpacing: 2,
-        mainAxisSpacing: 2,
-        childAspectRatio: 1,
-      ),
-      itemBuilder: (context, index) =>
-          CustomProductItem(productItemModel: productItems[index]),
-    );
+    List<CustomProductItemModel> productItems = [];
+    return BlocBuilder<GetProductCubit, GetProductStates>(
+        builder: (context, state) {
+      if (state is GetProductSuccessState) {
+        productItems = state.productItems;
+        return GridView.builder(
+          shrinkWrap: true,
+          physics: const NeverScrollableScrollPhysics(),
+          itemCount: productItems.length,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 2,
+            mainAxisSpacing: 2,
+            childAspectRatio: 1,
+          ),
+          itemBuilder: (context, index) =>
+              CustomProductItem(productItemModel: productItems[index]),
+        );
+      } else if (state is GetProductErrorState) {
+        return Center(
+          child: Text(state.errorMessageFromApi),
+        );
+      } else {
+        return const Center(child: CircularProgressIndicator());
+      }
+    });
   }
 }
