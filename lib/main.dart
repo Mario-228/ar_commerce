5353677c8b90750222011310872eabea7128d66d
MarioYoussef <marioyoussef08@gmail.com>
2024-12-08 01:33:03 +0200

    install supabase

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,17 @@ import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:graduation_project/core/utils/app_colors/app_colors.dart';
 import 'package:graduation_project/core/utils/app_routers/app_routers.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 // import 'package:graduation_project/features/login_feature/presentation/views/login_view.dart';
 // import 'package:graduation_project/features/onboarding_view/presentation/views/onboarding_view.dart';
 // import 'package:graduation_project/features/splash_view/presentation/views/splash_view.dart';
 
-void main() {
+Future<void> main() async {
+  await Supabase.initialize(
+    url: 'https://rvwbjhccaspfouncrsru.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ2d2JqaGNjYXNwZm91bmNyc3J1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM1MTMwODMsImV4cCI6MjA0OTA4OTA4M30.s_DlRcBQQyGDpl8wIUv-LompywYBiBvMl0dwsb9crCM',
+  );
   runApp(const MyApp());
 }
 
