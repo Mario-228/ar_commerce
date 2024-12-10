cd8c850e2b3a8c8196cdc89987245018a53a0b9d
MarioYoussef <marioyoussef08@gmail.com>
2024-12-10 22:55:46 +0200

    create signup user model and signup constants

---
+++ lib/features/signup_feature/data/models/sign_up_user_model.dart
@@ -0,0 +1,42 @@
+import 'package:graduation_project/features/signup_feature/data/repo/sign_up_repo_constants.dart';
+
+class SignUpUserModel {
+  String id;
+  String name;
+  String email;
+  String phone;
+  String password;
+  String userImageUrl;
+  String createdAt;
+
+  SignUpUserModel({
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.phone,
+    required this.password,
+    required this.userImageUrl,
+    required this.createdAt,
+  });
+
+  static SignUpUserModel fromJson(Map<String, dynamic> json) => SignUpUserModel(
+        id: json[SignUpRepoConstants.id],
+        name: json[SignUpRepoConstants.name],
+        email: json[SignUpRepoConstants.email],
+        phone: json[SignUpRepoConstants.phone] ?? "",
+        userImageUrl:
+            "https://img.freepik.com/free-vector/blue-circle-with-white-user_78370-4707.jpg?t=st=1733863368~exp=1733866968~hmac=dcf753cc48215353e01269e8d6aaf27adfa1e7eff7d23173fd74e6dda73b5dd2&w=740",
+        createdAt: DateTime.now().millisecondsSinceEpoch.toString(),
+        password: json[SignUpRepoConstants.password],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "name": name,
+        "email": email,
+        "phone": phone,
+        "password": password,
+        "userImageUrl": userImageUrl,
+        "createdAt": createdAt,
+      };
+}
---
+++ lib/features/signup_feature/data/repo/sign_up_repo_constants.dart
@@ -0,0 +1,9 @@
+abstract class SignUpRepoConstants {
+  static const String usersEndPoint = "AspNetUsers";
+  static const String id = "Id";
+  static const String name = "UserName";
+  static const String email = "Email";
+  static const String phone = "Phone";
+  static const String emailConfirmed = "EmailConfirmed";
+  static const String password = "password";
+}
