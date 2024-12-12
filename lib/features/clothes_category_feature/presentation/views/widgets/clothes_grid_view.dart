d4af4e5536df5ff0ab6facb2f1c4ba038d1a1d69
Kyria-Ashraf <keirayaashraf@gmail.com>
2024-12-12 04:04:13 +0200

    clothes products display

--- lib/features/clothes_category_feature/presentation/views/widgets/clothes_grid_view.dart
+++ lib/features/clothes_category_feature/presentation/views/widgets/clothes_grid_view.dart
@@ -1,83 +1,58 @@
 import 'package:flutter/material.dart';
-// import 'package:go_router/go_router.dart';
-// import 'package:graduation_project/core/utils/app_routers/app_routers.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:graduation_project/core/utils/custom_product_item_model/custom_product_item_model.dart';
+import 'package:graduation_project/features/clothes_category_feature/presentation/views_models/get_clothes_cubit/get_clothes_cubit.dart';
 import 'package:graduation_project/features/home_feature/presentation/widgets/custom_product_item.dart';
+import '../../views_models/get_clothes_cubit/get_clothes_states.dart';
 
 class ClothesProductsGridView extends StatelessWidget {
   const ClothesProductsGridView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    List<CustomProductItemModel> productItems = [
-      CustomProductItemModel(
-        id: 1,
-        name: 'Product 1',
-        price: 10,
-        pictureUrl: 'https://via.placeholder.com/150',
-        isFavorite: true,
-        productBrand: 'Brand 1',
-        productBrandId: 1,
-        quantity: 10,
-        productTypeId: 1,
-        productType: 'Type 1',
-        currency: 'L.E ',
-        description: 'Description 1',
-      ),
-      CustomProductItemModel(
-        id: 1,
-        name: 'Product 1',
-        price: 10,
-        pictureUrl: 'https://via.placeholder.com/150',
-        isFavorite: true,
-        productBrand: 'Brand 1',
-        productBrandId: 1,
-        quantity: 10,
-        productTypeId: 1,
-        productType: 'Type 1',
-        currency: 'L.E ',
-        description: 'Description 1',
-      ),
-      CustomProductItemModel(
-        id: 1,
-        name: 'Product 1',
-        price: 10,
-        pictureUrl: 'https://via.placeholder.com/150',
-        isFavorite: true,
-        productBrand: 'Brand 1',
-        productBrandId: 1,
-        quantity: 10,
-        productTypeId: 1,
-        productType: 'Type 1',
-        currency: 'L.E ',
-        description: 'Description 1',
-      ),
-      CustomProductItemModel(
-        id: 1,
-        name: 'Product 1',
-        price: 10,
-        pictureUrl: 'https://via.placeholder.com/150',
-        isFavorite: true,
-        productBrand: 'Brand 1',
-        productBrandId: 1,
-        quantity: 10,
-        productTypeId: 1,
-        productType: 'Type 1',
-        currency: 'L.E ',
-        description: 'Description 1',
-      ),
-    ];
-    return GridView.builder(
-      physics: const BouncingScrollPhysics(),
-      itemCount: 4,
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        crossAxisSpacing: 2,
-        mainAxisSpacing: 2,
-        childAspectRatio: 1,
-      ),
-      itemBuilder: (context, index) =>
-          CustomProductItem(productItemModel: productItems[index]),
-    );
+    List<CustomProductItemModel> productItems = [];
+
+    return BlocBuilder<GetClothesCubit, GetClothesStates>(
+        builder: (context, state) {
+      if (state is GetClothesSuccessState) {
+        productItems = state.clothesProducts;
+        return GridView.builder(
+          physics: const BouncingScrollPhysics(),
+          itemCount: productItems.length,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 2,
+            mainAxisSpacing: 2,
+            childAspectRatio: 1,
+          ),
+          itemBuilder: (context, index) =>
+              CustomProductItem(productItemModel: productItems[index]),
+        );
+      } else if (state is GetClothesFailedState) {
+        return Center(
+          child: Text(state.errorMsg),
+        );
+      } else {
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      }
+    });
   }
 }
+
+
+/*
+            return GridView.builder(
+              physics: const BouncingScrollPhysics(),
+              itemCount: 4,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 2,
+                mainAxisSpacing: 2,
+                childAspectRatio: 1,
+              ),
+              itemBuilder: (context, index) =>
+                  CustomProductItem(productItemModel: productItems[index]),
+            );
+*/
