8e3ff222bfa60b056107e95feb79128620fbd65d
MarioYoussef <marioyoussef08@gmail.com>
2025-01-05 03:56:59 +0200

    add profile view route

--- lib/core/utils/app_routers/app_routers.dart
+++ lib/core/utils/app_routers/app_routers.dart
@@ -16,6 +16,7 @@ import 'package:graduation_project/features/others_category_feature/presentation
 import 'package:graduation_project/features/password_reset_feature/presentation/views/password_reset_view.dart';
 import 'package:graduation_project/features/popular_category_feature/presentation/views/popular_category_view.dart';
 import 'package:graduation_project/features/product_details_feature/presentation/views/product_details_view.dart';
+import 'package:graduation_project/features/profile_feature/presentation/views/profile_view.dart';
 import 'package:graduation_project/features/signup_feature/data/repo/sign_up_repo_implementation.dart';
 import 'package:graduation_project/features/signup_feature/presentation/views/signup_view.dart';
 import 'package:graduation_project/features/signup_feature/presentation/views_models/sign_up_cubit/sign_up_cubit.dart';
@@ -34,6 +35,7 @@ abstract class AppRouters {
   static const String kOthersCategoryView = '/othersCategoryView';
   static const String kPopularCategoryView = '/popularCategoryView';
   static const String kProductDetailsView = '/productDetailsView';
+  static const String kProfileView = '/profileView';
 
   static final routers = GoRouter(
     routes: <RouteBase>[
@@ -105,6 +107,10 @@ abstract class AppRouters {
       GoRoute(
         path: kProductDetailsView,
         builder: (context, state) => const ProductDetailsView(),
+      ),
+      GoRoute(
+        path: kProfileView,
+        builder: (context, state) => const ProfileView(),
       ),
     ],
   );
