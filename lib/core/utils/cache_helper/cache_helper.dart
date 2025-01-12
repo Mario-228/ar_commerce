d4f479fd8aa29936ddfdd38e1e51a9bb97ecf578
Kyria-Ashraf <keirayaashraf@gmail.com>
2025-01-13 01:20:18 +0200

    change getData method

--- lib/core/utils/cache_helper/cache_helper.dart
+++ lib/core/utils/cache_helper/cache_helper.dart
@@ -12,30 +12,27 @@ class CacheHelper {
       await prefs.setDouble(key, value);
     } else if (value is bool) {
       await prefs.setBool(key, value);
+    } else if (value is List<String>) {
+      await prefs.setStringList(key, value);
+    } else {
+      throw Exception('Invalid value type');
     }
   }
 
-  Future<int?> getIntData(String key) async {
+  Future<T?> getData<T>(String key) async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
-    final result = prefs.getInt(key);
-    return result;
-  }
-
-  Future<String?> getStringData(String key) async {
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
-    final result = prefs.getString(key);
-    return result;
-  }
-
-  Future<double?> getDoubleData(String key) async {
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
-    final result = prefs.getDouble(key);
-    return result;
-  }
-
-  Future<bool?> getBoolData(String key) async {
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
-    final result = prefs.getBool(key);
-    return result;
+    if (T is String) {
+      return prefs.getString(key) as T;
+    } else if (T is int) {
+      return prefs.getInt(key) as T;
+    } else if (T is double) {
+      return prefs.getDouble(key) as T;
+    } else if (T is bool) {
+      return prefs.getBool(key) as T;
+    } else if (T is List<String>) {
+      return prefs.getStringList(key) as T;
+    } else {
+      throw Exception('Invalid Key');
+    }
   }
 }
