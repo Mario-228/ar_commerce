668eaf5e163dbb46752e430dd8b719019ab48d40
MarioYoussef <marioyoussef08@gmail.com>
2025-04-11 05:41:25 +0200

    add list view to cart view body

--- lib/features/cart_feature/presentation/views/widgets/cart_view_body.dart
+++ lib/features/cart_feature/presentation/views/widgets/cart_view_body.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:graduation_project_new_version/features/cart_feature/presentation/views/widgets/custom_item_list_view.dart';
 
 class CartViewBody extends StatelessWidget {
   const CartViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column();
+    return Column(
+      children: [
+        Expanded(
+          child: CustomItemCartListView(),
+        ),
+      ],
+    );
   }
 }
