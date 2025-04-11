cb4806cf94e8dfb28a5db24a1003593090a2d63f
MarioYoussef <marioyoussef08@gmail.com>
2025-04-11 03:41:08 +0200

    create cart view class

--- lib/features/cart_feature/presentation/views/cart_view.dart
+++ lib/features/cart_feature/presentation/views/cart_view.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:graduation_project_new_version/core/widgets/custom_appbar.dart';
+import 'package:graduation_project_new_version/features/cart_feature/presentation/views/widgets/cart_view_body.dart';
 
 class CartView extends StatelessWidget {
   const CartView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      appBar: CustomAppbar(title: "Cart"),
+      body: CartViewBody(),
+    );
   }
 }
