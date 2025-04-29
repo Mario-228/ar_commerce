91c1e3477aebdedd950bba377efbad2701ef58aa
MarioYoussef <marioyoussef08@gmail.com>
2025-04-29 13:05:54 +0300

    create checkout view

---
+++ lib/features/checkout_feature/presentation/views/widgets/checkout_view_body.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/widgets.dart';
+
+class CheckoutViewBody extends StatelessWidget {
+  const CheckoutViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [],
+    );
+  }
+}
