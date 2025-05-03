7aa88a693d7d53727f1375ee25cdceaf19d9d007
MarioYoussef <marioyoussef08@gmail.com>
2025-05-03 14:52:54 +0300

    create makePayment in checkout repo

--- lib/features/checkout_feature/data/repos/checkout_repo.dart
+++ lib/features/checkout_feature/data/repos/checkout_repo.dart
@@ -2,6 +2,10 @@ import 'package:dartz/dartz.dart';
 import 'package:graduation_project_new_version/core/errors/errors.dart';
 import 'package:graduation_project_new_version/features/checkout_feature/data/models/address_model.dart';
 
+import '../../../../core/utils/stripe_service/models/payment_intent_input_model/payment_intent_input_model.dart';
+
 abstract class CheckoutRepo {
   Future<Either<Errors, List<AddressModel>>> getAddresses();
+  Future<Either<Errors, void>> makePayment(
+      {required PaymentIntentInputModel model});
 }
