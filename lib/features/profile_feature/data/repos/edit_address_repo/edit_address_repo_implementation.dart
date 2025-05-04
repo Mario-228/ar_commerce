24cf0dcd36238b35870b6d00669897b553a9c878
MarioYoussef <marioyoussef08@gmail.com>
2025-05-04 19:50:52 +0300

    create edit address repo

---
+++ lib/features/profile_feature/data/repos/edit_address_repo/edit_address_repo_implementation.dart
@@ -0,0 +1,27 @@
+import 'package:dartz/dartz.dart';
+import 'package:graduation_project_new_version/core/errors/errors.dart';
+import 'package:graduation_project_new_version/core/utils/api_service/api_service.dart';
+import 'package:graduation_project_new_version/core/utils/api_service/base_url.dart';
+import 'package:graduation_project_new_version/core/utils/cache_helper/cache_helper.dart';
+import 'package:graduation_project_new_version/features/checkout_feature/data/models/address_model.dart';
+import 'package:graduation_project_new_version/features/profile_feature/data/models/edit_address_user_model.dart';
+import 'package:graduation_project_new_version/features/profile_feature/data/repos/edit_address_repo/edit_address_end_points.dart';
+import 'package:graduation_project_new_version/features/profile_feature/data/repos/edit_address_repo/edit_address_repo.dart';
+
+class EditAddressRepoImplementation implements EditAddressRepo {
+  @override
+  Future<Either<Errors, AddressModel>> editDeliveryAddress(
+      EditAddressUserModel model, int addressId) async {
+    try {
+      var response = await ApiService(BaseUrl.products).putData(
+        endPoint:
+            EditAddressEndPoints.editDeliveryAddress + addressId.toString(),
+        token: CacheHelper.getUserData().token,
+        data: model.toJson(),
+      );
+      return right(AddressModel.fromJson(response));
+    } on Exception catch (e) {
+      return left(ServerError.fromDioError(e));
+    }
+  }
+}
