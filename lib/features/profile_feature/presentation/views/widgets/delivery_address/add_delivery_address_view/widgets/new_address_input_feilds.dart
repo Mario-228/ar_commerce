db4e5974123216aa4a1a5f2e2c6517cdd669e4b3
MarioYoussef <marioyoussef08@gmail.com>
2025-05-07 06:09:55 +0300

    make the controllers of the add new address view non static

--- lib/features/profile_feature/presentation/views/widgets/delivery_address/add_delivery_address_view/widgets/new_address_input_feilds.dart
+++ lib/features/profile_feature/presentation/views/widgets/delivery_address/add_delivery_address_view/widgets/new_address_input_feilds.dart
@@ -1,5 +1,10 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:graduation_project_new_version/core/utils/location_service/location_service.dart';
 import 'package:graduation_project_new_version/core/widgets/custom_text_form_field.dart';
+import 'package:graduation_project_new_version/features/google_map_feature/presentation/views/google_maps_view.dart';
 import 'package:graduation_project_new_version/features/profile_feature/presentation/views_models/add_new_address_cubit/add_new_address_cubit.dart';
 
 class NewAddressInputFeilds extends StatelessWidget {
@@ -10,13 +15,13 @@ class NewAddressInputFeilds extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Form(
-      key: AddNewAddressCubit.formKey,
+      key: AddNewAddressCubit.get(context).formKey,
       child: Column(
         children: [
           CustomTextFormField(
             type: TextInputType.name,
             labelText: "Full Name",
-            controller: AddNewAddressCubit.name,
+            controller: AddNewAddressCubit.get(context).name,
             validator: (p0) {
               if (p0 == null || p0.isEmpty) {
                 return "Insert name";
@@ -29,7 +34,7 @@ class NewAddressInputFeilds extends StatelessWidget {
           CustomTextFormField(
             type: TextInputType.emailAddress,
             labelText: "Email",
-            controller: AddNewAddressCubit.email,
+            controller: AddNewAddressCubit.get(context).email,
             validator: (p0) {
               if (p0 == null || p0.isEmpty) {
                 return "Insert email";
@@ -42,7 +47,7 @@ class NewAddressInputFeilds extends StatelessWidget {
           CustomTextFormField(
             type: TextInputType.text,
             labelText: "Phone Number",
-            controller: AddNewAddressCubit.phone,
+            controller: AddNewAddressCubit.get(context).phone,
             validator: (p0) {
               if (p0 == null || p0.isEmpty) {
                 return "Insert phone number";
@@ -55,7 +60,7 @@ class NewAddressInputFeilds extends StatelessWidget {
           CustomTextFormField(
             type: TextInputType.streetAddress,
             labelText: "Address",
-            controller: AddNewAddressCubit.address,
+            controller: AddNewAddressCubit.get(context).address,
             validator: (p0) {
               if (p0 == null || p0.isEmpty) {
                 return "Insert address";
@@ -63,6 +68,13 @@ class NewAddressInputFeilds extends StatelessWidget {
                 return null;
               }
             },
+            icon: Icons.location_on_outlined,
+            onPressed: () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const GoogleMapsView(),
+              ),
+            ),
           ),
         ],
       ),
