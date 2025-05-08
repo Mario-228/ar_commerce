74160747d48396a72795727184cd2bb2cf45054f
Kyria-Ashraf <keirayaashraf@gmail.com>
2025-05-09 00:41:39 +0300

    create get order repo

---
+++ lib/features/profile_feature/data/repos/orders_profile_repo/order_profile_repo_implementation.dart
@@ -0,0 +1,33 @@
+import 'dart:developer';
+
+import 'package:dartz/dartz.dart';
+import 'package:graduation_project_new_version/core/errors/errors.dart';
+import 'package:graduation_project_new_version/core/utils/api_service/api_service.dart';
+import 'package:graduation_project_new_version/core/utils/api_service/base_url.dart';
+import 'package:graduation_project_new_version/core/utils/cache_helper/cache_helper.dart';
+import 'package:graduation_project_new_version/features/profile_feature/data/models/get_orders_model.dart';
+import 'package:graduation_project_new_version/features/profile_feature/data/repos/orders_profile_repo/orders_profile_repo.dart';
+import 'package:graduation_project_new_version/features/profile_feature/data/repos/orders_profile_repo/orders_profile_repo_endpoints.dart';
+
+class OrderProfileRepoImplementation implements OrdersProfileRepo {
+  @override
+  Future<Either<Errors, GetOrderModel>> getOrders() async {
+    try {
+      var response = await ApiService(BaseUrl.authentication).getWithToken(
+          OrdersProfileRepoEndpoints.orders, CacheHelper.getUserData().token);
+
+      //  List<OrderDetail> orders = [];
+      // for (var order in response['order']['order_detail']) {
+      //   orders.add(OrderDetail.fromJson(order));
+      // }
+      if (response['order'] == []) {
+        log('empty order');
+      }
+      return right(response['order']);
+    } catch (e) {
+      return left(
+        ServerError.fromDioError(e),
+      );
+    }
+  }
+}
---
+++ lib/features/profile_feature/data/repos/orders_profile_repo/orders_profile_repo_endpoints.dart
@@ -0,0 +1,3 @@
+abstract class OrdersProfileRepoEndpoints {
+  static const String orders = 'orders';
+}
