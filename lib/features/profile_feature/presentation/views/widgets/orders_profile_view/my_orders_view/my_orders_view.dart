845f0fd0edc67d66bd79f0f85ebd7c51475d29d6
Kyria-Ashraf <keirayaashraf@gmail.com>
2025-05-09 00:42:52 +0300

    edit my orders view to display orders as a pdf, and trigger get order cubit

--- lib/features/profile_feature/presentation/views/widgets/orders_profile_view/my_orders_view/my_orders_view.dart
+++ lib/features/profile_feature/presentation/views/widgets/orders_profile_view/my_orders_view/my_orders_view.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:graduation_project_new_version/features/profile_feature/presentation/views/widgets/orders_profile_view/my_orders_view/widgets/my_orders_view_body.dart';
-
-import '../../../../../../../core/utils/models/custom_product_item_model/custom_product_item_model.dart';
-import '../../../../../../../core/widgets/custom_appbar.dart';
+import 'package:graduation_project_new_version/features/profile_feature/presentation/views_models/get_orders_profile_cubit/get_order_profile_cubit.dart';
 
 class MyOrdersView extends StatelessWidget {
   const MyOrdersView({super.key});
@@ -10,28 +9,10 @@ class MyOrdersView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const CustomAppbar(
-        title: "My Orders",
-      ),
-      body: MyOrdersViewBody(
-        productItemsList: [
-          CustomProductItemModel(
-              id: 1,
-              name: "Product",
-              description: "product description",
-              pictureUrl:
-                  "https://media.istockphoto.com/id/1222357475/vector/image-preview-icon-picture-placeholder-for-website-or-ui-ux-design-vector-illustration.jpg?s=2048x2048&w=is&k=20&c=CJLIU6nIISsrHLTVO04nxIH2zVaKbnUeUXp7PnpM2h4=",
-              price: 500.0,
-              currency: "L.E",
-              isFavorite: false,
-              isLike: false,
-              productBrandId: 0,
-              productTypeId: 0,
-              quantity: 5,
-              productBrand: "",
-              productType: "",
-              image3DUrl: ""),
-        ],
+      appBar: AppBar(title: const Text("Order Invoice")),
+      body: BlocProvider(
+        create: (context) => GetOrderProfileCubit()..getOrders(),
+        child: MyOrdersViewBody(),
       ),
     );
   }
--- lib/features/profile_feature/presentation/views/widgets/orders_profile_view/my_orders_view/widgets/my_orders_view_body.dart
+++ lib/features/profile_feature/presentation/views/widgets/orders_profile_view/my_orders_view/widgets/my_orders_view_body.dart
@@ -1,23 +1,97 @@
-import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
+import 'dart:typed_data';
 
-import '../../../../../../../../core/utils/app_routers/app_routers.dart';
-import '../../../../../../../../core/utils/models/custom_product_item_model/custom_product_item_model.dart';
-import '../../../../../../../../core/widgets/order_list_tile.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:graduation_project_new_version/features/profile_feature/presentation/views_models/get_orders_profile_cubit/get_order_profile_cubit.dart';
+import 'package:graduation_project_new_version/features/profile_feature/presentation/views_models/get_orders_profile_cubit/get_order_profile_states.dart';
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pw;
+import 'package:printing/printing.dart';
+
+import '../../../../../../data/models/get_orders_model.dart';
 
 class MyOrdersViewBody extends StatelessWidget {
-  const MyOrdersViewBody({super.key, required this.productItemsList});
-  final List<CustomProductItemModel> productItemsList;
+  const MyOrdersViewBody({
+    super.key,
+  });
+
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      physics: const BouncingScrollPhysics(),
-      itemBuilder: (context, index) => OrderListTile(
-        productItemModel: productItemsList[index],
-        onTap: () => GoRouter.of(context).push(AppRouters.kMyOrderDetailsView,
-            extra: productItemsList[index]),
-      ),
-      itemCount: productItemsList.length,
+    return BlocBuilder<GetOrderProfileCubit, GetOrderProfileStates>(
+      builder: (context, state) {
+        if (state is GetOrderProfileSuccessState) {
+          return PdfPreview(
+            canDebug: false,
+            allowSharing: true,
+            allowPrinting: true,
+            useActions: true,
+            scrollViewDecoration: BoxDecoration(color: Colors.grey[100]),
+            build: (format) => _generatePdf(format, state.order),
+          );
+        } else if (state is GetOrderProfileErrorState) {
+          return Center(
+            child: Text(
+              state.error,
+              style: const TextStyle(color: Colors.red, fontSize: 20),
+            ),
+          );
+        } else {
+          return const Center(child: CircularProgressIndicator());
+        }
+      },
     );
+  }
+
+  Future<Uint8List> _generatePdf(
+      PdfPageFormat format, GetOrderModel order) async {
+    final pdf = pw.Document();
+
+    pdf.addPage(
+      pw.MultiPage(
+        pageFormat: format,
+        margin: const pw.EdgeInsets.all(24),
+        build: (context) => [
+          pw.Header(
+            level: 0,
+            child: pw.Text("Order Invoice",
+                style:
+                    pw.TextStyle(fontSize: 26, fontWeight: pw.FontWeight.bold)),
+          ),
+          pw.SizedBox(height: 10),
+          pw.Text("Customer Name: ${order.name}"),
+          pw.Text("Email: ${order.email}"),
+          pw.Text("Phone: ${order.phone}"),
+          pw.Text("Address: ${order.address}"),
+          pw.SizedBox(height: 12),
+          pw.Text("Payment Method: ${order.paymentMethod}"),
+          pw.Text("Transaction ID: ${order.transactionId}"),
+          pw.Text("Order Status: ${order.status}"),
+          pw.Text("Total Amount: ${order.total.toStringAsFixed(2)} EGP"),
+          pw.SizedBox(height: 20),
+          pw.Text("Order Details",
+              style:
+                  pw.TextStyle(fontSize: 18, fontWeight: pw.FontWeight.bold)),
+          pw.SizedBox(height: 10),
+          pw.TableHelper.fromTextArray(
+            border: null,
+            cellAlignment: pw.Alignment.centerLeft,
+            headerStyle: pw.TextStyle(fontWeight: pw.FontWeight.bold),
+            headers: ['Product ID', 'Quantity', 'Price (EGP)'],
+            data: order.orderDetail
+                .map((item) => [
+                      item.productId.toString(),
+                      item.quantity.toString(),
+                      item.price.toStringAsFixed(2),
+                    ])
+                .toList(),
+          ),
+          pw.SizedBox(height: 30),
+          pw.Text("Thank you for your order!",
+              style: pw.TextStyle(fontStyle: pw.FontStyle.italic)),
+        ],
+      ),
+    );
+
+    return pdf.save();
   }
 }
