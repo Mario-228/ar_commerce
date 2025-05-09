c897b2bb2095b0d16b2083b1100f229ca9f7473b
MarioYoussef <marioyoussef08@gmail.com>
2025-05-09 07:17:59 +0300

    create my pending orders view bloc builder

---
+++ lib/features/profile_feature/presentation/views/widgets/orders_profile_view/my_orders_view/widgets/my_pending_orders_view_bloc_builder.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:graduation_project_new_version/features/profile_feature/presentation/views/widgets/orders_profile_view/my_orders_view/widgets/order_detail_item.dart';
+import '../../../../../../../../core/utils/app_colors/app_colors.dart';
+import '../../../../../../../../core/utils/font_styles/font_styles.dart';
+import '../../../../../views_models/get_orders_profile_cubit/get_order_profile_cubit.dart';
+import '../../../../../views_models/get_orders_profile_cubit/get_order_profile_states.dart';
+
+class MyPendingOrdersViewBlocBuilder extends StatelessWidget {
+  const MyPendingOrdersViewBlocBuilder({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<GetOrderProfileCubit, GetOrderProfileStates>(
+      builder: (context, state) {
+        if (state is GetOrderProfileSuccessState) {
+          return ListView.separated(
+            padding: const EdgeInsets.all(20.0),
+            itemCount: state.pendingOrders.length,
+            physics: const BouncingScrollPhysics(),
+            separatorBuilder: (context, index) => const SizedBox(height: 10.0),
+            itemBuilder: (context, index) => OrderDetailItem(
+              order: state.pendingOrders[index],
+            ),
+          );
+        } else if (state is GetOrderProfileLoadingState) {
+          return Center(child: CircularProgressIndicator());
+        } else {
+          return const CircleAvatar(
+            radius: 80,
+            backgroundColor: AppColors.darkGreen,
+            child: Text("No Pending Orders", style: FontStyles.textStyleBold19),
+          );
+        }
+      },
+    );
+  }
+}
