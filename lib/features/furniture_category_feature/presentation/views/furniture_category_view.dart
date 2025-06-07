60172a9a1ae55296c5daf29b28dd7729afb959d4
MarioYoussef <marioyoussef08@gmail.com>
2025-06-07 03:57:17 +0300

    improve the performance (get furniture products)

--- lib/features/furniture_category_feature/presentation/views/furniture_category_view.dart
+++ lib/features/furniture_category_feature/presentation/views/furniture_category_view.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:graduation_project_new_version/core/utils/get_it_service/get_it_service.dart';
 import '../../../../core/widgets/category_app_bar.dart';
 import '../../data/repos/furniture_repo_endpoints.dart';
-import '../../data/repos/furniture_repo_implementation.dart';
 import '../views_models/get_furniture_cubit/get_furniture_cubit.dart';
 import 'widgets/furniture_grid_view.dart';
 
@@ -13,8 +13,8 @@ class FurnitureCategoryView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const CategoryAppbar(title: "Furniture"),
-      body: BlocProvider(
-        create: (context) => GetFurnitureCubit(FurnitureRepoImplementation())
+      body: BlocProvider.value(
+        value: GetItService.getIt.get<GetFurnitureCubit>()
           ..getFurnitureProducts(
               endPoint:
                   FurnitureRepoEndpoints.product + FurnitureRepoEndpoints.id),
--- lib/features/furniture_category_feature/presentation/views_models/get_furniture_cubit/get_furniture_cubit.dart
+++ lib/features/furniture_category_feature/presentation/views_models/get_furniture_cubit/get_furniture_cubit.dart
@@ -9,15 +9,17 @@ class GetFurnitureCubit extends Cubit<GetFurnitureStates> {
 
   static GetFurnitureCubit get(BuildContext context) =>
       BlocProvider.of(context);
-
+  bool isLoaded = false;
   Future<void> getFurnitureProducts({required String endPoint}) async {
+    if (isLoaded) return;
     emit(GetFurnitureLoadingState());
     var result = await furnitureRepo.getFurnitureProducts(endPoint: endPoint);
     result.fold(
       (error) => emit(GetFurnitureFailedState(errorMsg: error.errorMessage)),
-      (data) => emit(
-        GetFurnitureSuccessState(furnitureProducts: data),
-      ),
+      (data) {
+        isLoaded = true;
+        emit(GetFurnitureSuccessState(furnitureProducts: data));
+      },
     );
   }
 }
