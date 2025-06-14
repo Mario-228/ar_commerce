0ad5e9904499f9ac5856e3b043d9934e0b0731fb
MarioYoussef <marioyoussef08@gmail.com>
2025-06-14 04:23:19 +0300

    create stripeGetData method & edit postData parameters

--- lib/core/utils/api_service/api_service.dart
+++ lib/core/utils/api_service/api_service.dart
@@ -49,7 +49,7 @@ class ApiService {
   }
 
   Future<Map<String, dynamic>> postData(
-      String endPoint, Map<String, dynamic> query) async {
+      String endPoint, Map<String, dynamic>? query) async {
     var response = await dioHelper.post(
       endPoint,
       queryParameters: query,
@@ -214,6 +214,25 @@ class ApiService {
     var response = await dioHelper.post(
       endPoint,
       queryParameters: parameters,
+      options: Options(
+        contentType: contentType,
+        headers: headers ??
+            {
+              "Authorization": "Bearer $token",
+            },
+      ),
+    );
+    return response;
+  }
+
+  Future<Response> stripeGetData({
+    required String endPoint,
+    required String token,
+    String? contentType,
+    Map<String, dynamic>? headers,
+  }) async {
+    var response = await dioHelper.get(
+      endPoint,
       options: Options(
         contentType: contentType,
         headers: headers ??
