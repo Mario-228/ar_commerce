5f9031e57a607e7006fb0c32ed5bb43af868b2e2
Kyria-Ashraf <keirayaashraf@gmail.com>
2025-06-16 02:08:33 +0300

    update signup view

--- lib/features/signup_feature/presentation/views/widgets/signup_gender_section.dart
+++ lib/features/signup_feature/presentation/views/widgets/signup_gender_section.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:graduation_project_new_version/core/utils/font_styles/font_styles.dart';
 import 'package:graduation_project_new_version/features/signup_feature/presentation/views_models/sign_up_cubit/sign_up_cubit.dart';
 import 'package:radio_group_v2/widgets/views/radio_group.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class SignupGenderSection extends StatelessWidget {
   const SignupGenderSection({super.key});
@@ -11,11 +12,12 @@ class SignupGenderSection extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text("Select Gender:", style: FontStyles.textStyleBold22),
+        Text(AppLocalizations.of(context)!.selectGender,
+            style: FontStyles.textStyleBold22),
         RadioGroup(
           values: [
-            "male",
-            "female",
+            AppLocalizations.of(context)!.male,
+            AppLocalizations.of(context)!.female,
           ],
           controller: SignUpCubit.get(context).genderController,
         ),
